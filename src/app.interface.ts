8d7585939467c09d5e1e4edfc1a180b11d7ac3a4
rahultagadghar <rahultagadghar@gmail.com>
2019-12-08 12:59:25 +0530

    error handler and finish metod added

---
+++ src/app.interface.ts
@@ -0,0 +1,5 @@
+import { Response } from "express";
+
+export interface ExpressResponse extends Response {
+    finish: (obj?: object, msg?: string, httpStatus?: number, errors?: object) => void
+}
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { config } from 'dotenv'
+config()
+import express, { NextFunction } from 'express'
+const app = express();
+import { attachFinishMethod, expressErrorHandler, ExpressError } from './util';
+import { ExpressResponse } from './app.interface';
+const { log } = console;
+const port = process.env.PORT;
+app.listen(port, () => log("server on : ", port));
+
+/* 
+    INFO : attachFinishMethod callback attaches finish method (req.finish) to every incoming request  
+*/
+app.use(attachFinishMethod);
+
+app.get("/", (req, res: ExpressResponse, next) => {
+    try {
+        /* 
+            INFO : Message as second argument is optional
+        */
+        const result = {
+            isUser: true,
+        }
+
+        res.finish(result, "Fetched User Info!");
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.get("/mockErrorPath", (req, res: ExpressResponse, next) => {
+    try {
+        /* 
+            INFO : httpStatusCode as second argument is optional by default it is 400
+        */
+        throw new ExpressError("Its Forbidden", 403)
+    } catch (error) {
+        next(error);
+    }
+});
+
+/* 
+    INFO : Standard error handler,
+           When we execute next() from api it is passed to next middleware 
+           which is in our case expressErrorHandler and request is terminated !
+*/
+app.use(expressErrorHandler);
